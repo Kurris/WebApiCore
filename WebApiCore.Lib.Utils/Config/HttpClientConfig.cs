eed218ffc675d4c3aee12021b14b91f959c040f5
Ligy <791444095@qq.com>
2021-01-13 21:50:11 +0800

    添加httpclient配置类

---
+++ WebApiCore.Lib.Utils/Config/HttpClientConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebApiCore.Lib.Utils.Config
+{
+    public class HttpClientConfig
+    {
+        public string BaseUrl { get; set; }
+        public int Timeout { get; set; }
+    }
+}
